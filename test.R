3a7c6939f8767687f017d6083cbca44ba2c1a751
Alex Drożdż <alexdrozdz@192.168.1.2>
2022-05-11 21:32:07 +0200

    project initialized

---
+++ test.R
@@ -0,0 +1,8 @@
+library(dplyr)
+
+usa_pop <- read.csv("data/China/china_population.csv")
+china_pop <- read.csv("data/USA/population_usa.csv")
+
+china_pop
+
+# mark code and then go to "Code > Reformat code" to use code auto-format in R 
