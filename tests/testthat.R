b3740803918b1bbaedfb79bfbe276b3618a99312
Alex Drożdż <alexdrozdz@192.168.1.2>
2022-06-26 21:49:15 +0200

    app refactor and ci

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,9 +1,7 @@
 library(testthat)
-library(usethis)
 library(devtools)
-library(shinytest)
 
-source("data_processing.R")
+source("../../data_processing.R")
 
 test_check("testthat")
 
--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -1,7 +1,9 @@
+library(shinytest)
+
 # Shiny app tests
 context("Shiny App Tests")
 
-app <- ShinyDriver$new("app.R")
+app <- ShinyDriver$new("../../app.R")
 testthat::test_that('app operation', {
   expect_identical(app$getTitle(), "China vs. USA Pop.")
   expect_identical(app$getUrl(), "http://0.0.0.0:8080/")
