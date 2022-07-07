17fa18d10a69c58fb161d1353d102ee7c378511c
Adrianna xD <adaprzybylska@wp.pl>
2022-07-07 21:08:14 +0200

    3 tab

--- server.R
+++ server.R
@@ -5,108 +5,240 @@ library(shiny)
 library(shinydashboard)
 library(ggplot2)
 library(usethis)
+library(plotly)
 
 source("data_processing.R")
 
 server <- function(input, output) {
   # plot of China population
-  output$plot_pop_china <- renderPlot({
-    ggplot(china_pop, aes(x = Year, y = Population / 1000000)) +
-      geom_point(color = "red") +
-      ylim(0, 1500) +
-      ylab("Population in MLN") +
-      ggtitle("What was the China population?") +
-      theme_light()
+  output$plot_pop_china <- renderPlotly({
+    ggplotly(
+      ggplot(china_pop, aes(x = Year, y = Population / 1000000)) +
+        geom_point(color = "red") +
+        ylim(0, 1500) +
+        ylab("Population in MLN") +
+        ggtitle("What was the China population?") +
+        theme_light()
+    )
   })
 
   # plot of US population
-  output$plot_pop_usa <- renderPlot({
-    ggplot(usa_pop, aes(x = Year, y = Population / 1000000)) +
-      geom_point(color = "blue") +
-      ylim(0, 1500) +
-      ylab("Population in MLN") +
-      ggtitle("What was the USA population?") +
-      theme_light()
+  output$plot_pop_usa <- renderPlotly({
+    ggplotly(
+      ggplot(usa_pop, aes(x = Year, y = Population / 1000000)) +
+        geom_point(color = "blue") +
+        ylim(0, 1500) +
+        ylab("Population in MLN") +
+        ggtitle("What was the USA population?") +
+        theme_light()
+    )
   })
 
   # plot of migrants in China and US
-  output$plot_migrants <- renderPlot({
-    ggplot() +
-      geom_line(
-        data = usa_china_pop,
-        aes(
-          x = Year,
-          y = Migrants_net.USA / 1000000,
-          colour = "USA"
-        )
-      ) +
-      geom_line(
-        data = usa_china_pop,
-        aes(
-          x = Year,
-          y = Migrants_net.China / 1000000,
-          colour = "China"
-        )
-      ) +
-      scale_color_manual(
-        name = "Country",
-        values = c(
-          "USA" = "blue",
-          "China" = "red"
-        )
-      ) +
-      ylab("Migrants in MLN") +
-      ggtitle("How many migrants where in given year?") +
-      theme_light()
+  output$plot_migrants <- renderPlotly({
+    ggplotly(
+      ggplot() +
+        geom_line(
+          data = usa_china_pop,
+          aes(
+            x = Year,
+            y = Migrants_net.USA / 1000000,
+            colour = "USA"
+          )
+        ) +
+        geom_line(
+          data = usa_china_pop,
+          aes(
+            x = Year,
+            y = Migrants_net.China / 1000000,
+            colour = "China"
+          )
+        ) +
+        scale_color_manual(
+          name = "Country",
+          values = c(
+            "USA" = "blue",
+            "China" = "red"
+          )
+        ) +
+        ylab("Migrants in MLN") +
+        ggtitle("How many migrants where in given year?") +
+        theme_light()
+    )
   })
 
   # plot of density in China and US
-  output$plot_density <- renderPlot({
-    ggplot() +
-      geom_line(data = usa_china_pop, aes(
-        x = Year,
-        y = Density_p_km2.USA,
-        colour = "USA"
-      )) +
-      geom_line(data = usa_china_pop, aes(
-        x = Year,
-        y = Density_p_km2.China,
-        colour = "China"
-      )) +
-      scale_color_manual(
-        name = "Country",
-        values = c(
-          "USA" = "blue",
-          "China" = "red"
-        )
-      ) +
-      ylab("Density [p/km2]") +
-      ggtitle("What is the population to the size of the country?") +
-      theme_light()
+  output$plot_density <- renderPlotly({
+    ggplotly(
+      ggplot() +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Density_p_km2.USA,
+          colour = "USA"
+        )) +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Density_p_km2.China,
+          colour = "China"
+        )) +
+        scale_color_manual(
+          name = "Country",
+          values = c(
+            "USA" = "blue",
+            "China" = "red"
+          )
+        ) +
+        ylab("Density [p/km2]") +
+        ggtitle("What is the population to the size of the country?") +
+        theme_light()
+    )
   })
 
   # plot of median age in China and US
-  output$plot_median_age <- renderPlot({
-    ggplot() +
-      geom_line(data = usa_china_pop, aes(
-        x = Year,
-        y = Median_age.USA,
-        colour = "USA"
-      )) +
-      geom_line(data = usa_china_pop, aes(
-        x = Year,
-        y = Median_age.China,
-        colour = "China"
-      )) +
-      scale_color_manual(
-        name = "Country",
-        values = c(
-          "USA" = "blue",
-          "China" = "red"
-        )
-      ) +
-      ylab("Median age") +
-      ggtitle("Is it an aging society?") +
-      theme_light()
+  output$plot_median_age <- renderPlotly({
+    ggplotly(
+      ggplot() +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Median_age.USA,
+          colour = "USA"
+        )) +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Median_age.China,
+          colour = "China"
+        )) +
+        scale_color_manual(
+          name = "Country",
+          values = c(
+            "USA" = "blue",
+            "China" = "red"
+          )
+        ) +
+        ylab("Median age") +
+        ggtitle("Is it an aging society?") +
+        theme_light()
+    )
   })
+  
+  
+  plot_forecast_population <- ggplotly(
+      ggplot() +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Population.USA / 1000000,
+          colour = "USA"
+        )) +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Population.China / 1000000,
+          colour = "China"
+        )) +
+        geom_line(data = usa_china_for, aes(
+          x = Year,
+          y = Population.USA / 1000000,
+          colour = "USA"
+        ),linetype="dashed"
+        ) +
+        geom_line(data = usa_china_for, aes(
+          x = Year,
+          y = Population.China / 1000000,
+          colour = "China"
+        ),linetype="dashed"
+        ) +
+        scale_color_manual(
+          name = "Country",
+          values = c(
+            "USA" = "blue",
+            "China" = "red"
+          )
+        ) +
+        ylab("Population forecast") +
+        ggtitle("How the population will probably look like in the future?") +
+        theme_light()
+    )
+  
+  plot_forecast_density <- ggplotly(
+      ggplot() +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Density_p_km2.USA,
+          colour = "USA"
+        )) +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Density_p_km2.China,
+          colour = "China"
+        )) +
+        geom_line(data = usa_china_for, aes(
+          x = Year,
+          y = Density_p_km2.USA,
+          colour = "USA"
+        ),linetype="dashed"
+        ) +
+        geom_line(data = usa_china_for, aes(
+          x = Year,
+          y = Density_p_km2.China,
+          colour = "China"
+        ),linetype="dashed"
+        ) +
+        scale_color_manual(
+          name = "Country",
+          values = c(
+            "USA" = "blue",
+            "China" = "red"
+          )
+        ) +
+        ylab("Density forecast") +
+        ggtitle("How the density will probably look like in the future?") +
+        theme_light()
+    )
+  
+  plot_forecast_median_age <-ggplotly(
+      ggplot() +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Median_age.USA,
+          colour = "USA"
+        )) +
+        geom_line(data = usa_china_pop, aes(
+          x = Year,
+          y = Median_age.China,
+          colour = "China"
+        )) +
+        geom_line(data = usa_china_for, aes(
+          x = Year,
+          y = Median_age.USA,
+          colour = "USA"
+        ),linetype="dashed"
+        ) +
+        geom_line(data = usa_china_for, aes(
+          x = Year,
+          y = Median_age.China,
+          colour = "China"
+        ),linetype="dashed"
+        ) +
+        scale_color_manual(
+          name = "Country",
+          values = c(
+            "USA" = "blue",
+            "China" = "red"
+          )
+        ) +
+        ylab("Median age forecast") +
+        ggtitle("How the median age will probably look like in the future?") +
+        theme_light()
+    )
+  
+  output$plot <- renderPlotly({
+    if(input$Parameter == "Population"){
+      plot_forecast_population
+    }else if(input$Parameter == "Density"){
+      plot_forecast_density
+    }else{
+      plot_forecast_median_age
+    }
+  })
+    
 }
+
--- ui.R
+++ ui.R
@@ -9,17 +9,19 @@ ui <- dashboardPage(
     # this tab contain overall business inforation
     menuItem("Population overall compare", tabName = "population"),
     # this tab contain inforation about migrants
-    menuItem("Detail information", tabName = "detail")
+    menuItem("Detail information", tabName = "detail"),
+    # this tab contain forecast for 3 parameters
+    menuItem("Forecast data", tabName = "forecast")
   )),
   dashboardBody(tabItems(
     tabItem(
       "population",
       fluidRow(
         box(
-          plotOutput("plot_pop_china", height = 350)
+          plotlyOutput("plot_pop_china", height = 350)
         ),
         box(
-          plotOutput("plot_pop_usa", height = 350)
+          plotlyOutput("plot_pop_usa", height = 350)
         )
       ),
       fluidRow(
@@ -37,7 +39,7 @@ ui <- dashboardPage(
     tabItem(
       "detail",
       fluidRow(
-        box(plotOutput("plot_median_age", height = 250)),
+        box(plotlyOutput("plot_median_age", height = 250)),
         box(
           background = "light-blue",
           "Median age in US was almost 2 times higher in year 1950.
@@ -46,7 +48,7 @@ ui <- dashboardPage(
         )
       ),
       fluidRow(
-        box(plotOutput("plot_migrants", height = 250)),
+        box(plotlyOutput("plot_migrants", height = 250)),
         box(
           background = "light-blue",
           "Number of people who migrate from US is growing. There was
@@ -57,7 +59,7 @@ ui <- dashboardPage(
         )
       ),
       fluidRow(
-        box(plotOutput("plot_density", height = 250)),
+        box(plotlyOutput("plot_density", height = 250)),
         box(
           background = "light-blue",
           "In China the density in 1950 year was about 2 times higher
@@ -65,6 +67,20 @@ ui <- dashboardPage(
               countries. However, in the China this increase was much faster."
         )
       )
+    ),
+    tabItem(
+      "forecast",
+      fluidPage(
+        selectInput("Parameter", 
+                    "Choose a Parameter:",
+                    choices = list("Population", "Density", "Median age"),
+                    selected = "Population"
+        ),
+        #actionButton("add_plot", "See the plot"),
+        
+        plotlyOutput("plot")
+      )
     )
   ))
 )
+
